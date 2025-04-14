1416e714f21330739603d2f183fb270e7357d00c
Albero Mandlate <alberto.mandlate@outlook.com>
2025-04-14 22:01:24 +0200

    Adicionar classes Clientes e Cliente para gerenciamento de dados
    Esta atualização introduz duas novas classes: `Clientes` e `Cliente`. A classe `Clientes` gerencia uma lista de instâncias de `Cliente`, incluindo um cliente padrão chamado "Consumidor Final". A classe `Cliente` é um registro que contém propriedades como `Id`, `Nome`, `NUIT`, `Endereco`, `Telefone`, `Email`, `Pais` e uma propriedade calculada `IsConsumidorFinal`. Além disso, a classe `Clientes` oferece métodos para recuperar todos os clientes e buscar um cliente específico pelo ID.

---
+++ samples/SAF-T.Examples/SampleData/Entities/Clientes.cs
@@ -0,0 +1,47 @@
+﻿using SAFT.Mozambique.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAFT.Examples.SampleData.Entities
+{
+    public class Clientes
+    {
+        private List<Cliente> _clientes =
+        [
+            new Cliente
+            {
+                Id = "Consumidor Final",
+                Nome = "Consumidor Final",
+                NUIT = "000000000"
+            },
+            new Cliente
+            {
+                Id = "1",
+                Nome = "Cliente 1",
+                NUIT = "123456789",
+                Endereco = "Rua A, 123",
+                Telefone = "+258-8X123456789"
+            },
+            new Cliente{
+                Id = "2",
+                Nome = "Cliente 2",
+                NUIT = "987654321",
+                Endereco = "Avenida B, 456",
+                Telefone = "+258-8X987654321"
+            }
+        ];
+
+        public List<Cliente> GetClientes()
+        {
+            return _clientes;
+        }
+
+        public Cliente? GetCliente(string id)
+        {
+            return _clientes.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
---
+++ src/SAF-T.Mozambique/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAFT.Mozambique.Models
+{
+    public record class Cliente
+    {
+        public string Id { get; init; } = string.Empty;
+        public string? Nome { get; init; } = string.Empty;
+        public string? NUIT { get; init; } = string.Empty;
+        public string? Endereco { get; init; } = string.Empty;
+        public string? Telefone { get; init; } = string.Empty;
+        public string? Email { get; init; } = string.Empty;
+        public string? Pais { get; init; } = "MZ";
+        public bool IsConsumidorFinal { get => Id == "Consumidor Final"; }
+    }
+}
