f4919501862bca916ebbcc9fd232be1b019c4005
Alberto Cláudio Mandlate <alberto.mandlate@outlook.com>
2025-05-13 21:22:20 +0200

    Refatoração dos métodos de exportação e importação PEM
    Simplificação dos métodos `ExportPublicKeyToPem` e `ExportPrivateKeyToPem` para chamadas diretas a métodos específicos, eliminando a manipulação manual de bytes e a conversão para Base64. Os métodos `ImportPublicKey` e `ImportPrivateKey` foram reduzidos a uma única linha, removendo a lógica de decodificação do PEM. O método `DecodePem` foi removido, pois não é mais necessário com as novas implementações.

--- src/SAF-T.Cryptography/KeyManagement/PemUtils.cs
+++ src/SAF-T.Cryptography/KeyManagement/PemUtils.cs
@@ -4,59 +4,12 @@ namespace Simansoft.SAFT.Cryptography.KeyManagement
 {
     public static class PemUtils
     {
-        public static string ExportPublicKeyToPem(RSA rsa)
-        {
-            //var publicKeyBytes = rsa.ExportSubjectPublicKeyInfo();
-            //var base64 = Convert.ToBase64String(publicKeyBytes, Base64FormattingOptions.InsertLineBreaks);
+        public static string ExportPublicKeyToPem(RSA rsa) => rsa.ExportSubjectPublicKeyInfoPem();
 
-            //return "-----BEGIN PUBLIC KEY-----\n" + base64 + "\n-----END PUBLIC KEY-----";
-            return rsa.ExportSubjectPublicKeyInfoPem();
-        }
+        public static string ExportPrivateKeyToPem(RSA rsa) => rsa.ExportRSAPrivateKeyPem();
 
-        public static string ExportPrivateKeyToPem(RSA rsa)
-        {
-            //var privateKeyBytes = rsa.ExportPkcs8PrivateKey();
-            //var base64 = Convert.ToBase64String(privateKeyBytes, Base64FormattingOptions.InsertLineBreaks);
+        public static void ImportPublicKey(RSA rsa, string pem) => rsa.ImportFromPem(pem);
 
-            //return "-----BEGIN PRIVATE KEY-----\n" + base64 + "\n-----END PRIVATE KEY-----";
-            
-            return rsa.ExportRSAPrivateKeyPem();
-        }
-
-        //public static RSA ImportPublicKey(int keySize, string pem)
-        public static void ImportPublicKey(RSA rsa, string pem)
-        {
-            //var publicKeyBytes = DecodePem(pem, "PUBLIC KEY");
-            //RSA rsa = RSA.Create(keySize);
-            //rsa.ImportSubjectPublicKeyInfo(publicKeyBytes, out _);
-            rsa.ImportFromPem(pem);
-            //return rsa;
-        }
-
-        //public static RSA ImportPrivateKey(int keySize, string pem)
-        public static void ImportPrivateKey(RSA rsa, string pem)
-        {
-            //var privateKeyBytes = DecodePem(pem, "PRIVATE KEY");
-            //RSA rsa = RSA.Create(keySize);
-            //rsa.ImportPkcs8PrivateKey(privateKeyBytes, out _);
-            rsa.ImportFromPem(pem.ToCharArray());
-            //return rsa;
-        }
-
-        private static byte[] DecodePem(string pem, string section)
-        {
-            string header = $"-----BEGIN {section}-----";
-            string footer = $"-----END {section}-----";
-
-            int start = pem.IndexOf(header, StringComparison.Ordinal);
-            if (start < 0) throw new InvalidOperationException($"Header {header} não encontrado");
-
-            int end = pem.IndexOf(footer, start, StringComparison.Ordinal);
-            if (end < 0) throw new InvalidOperationException($"Footer {footer} não encontrado");
-
-            string base64 = pem.Substring(start + header.Length, end - start - header.Length);
-            //return Convert.FromBase64String(base64.Replace("\n", "").Replace("\r", ""));
-            return Convert.FromBase64String(base64);
-        }
+        public static void ImportPrivateKey(RSA rsa, string pem) => rsa.ImportFromPem(pem.ToCharArray());
     }
 }
