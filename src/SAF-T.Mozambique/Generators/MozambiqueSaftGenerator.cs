4ccbf749ca4b63c820ea61e2be0d8f26243b80c4
Alberto Cláudio Mandlate <alberto.mandlate@outlook.com>
2025-06-07 10:44:19 +0200

    Atualiza taxa de câmbio fixa para variável
    Adiciona a variável `taxaFixaCambio` inicializada com `1d` para definir a taxa de câmbio fixa. Substitui o valor literal "1" na célula 29 pela nova variável, melhorando a legibilidade e a manutenção do código.

--- src/SAF-T.Mozambique/Generators/MozambiqueSaftGenerator.cs
+++ src/SAF-T.Mozambique/Generators/MozambiqueSaftGenerator.cs
@@ -244,6 +244,7 @@ namespace Simansoft.SAFT.Mozambique.Generators
         {
             try
             {
+                double taxaFixaCambio = 1d;
                 if (auditFile == null)
                 {
                     throw new ArgumentNullException(nameof(auditFile), "O objeto AuditFile não pode ser nulo.");
@@ -345,7 +346,7 @@ namespace Simansoft.SAFT.Mozambique.Generators
                     row.CreateCell(26).SetCellValue(factura.DocumentTotals?.TaxPayable.ToString() ?? "0");
                     row.CreateCell(27).SetCellValue(factura.DocumentTotals?.GrossTotal.ToString() ?? "0");
                     row.CreateCell(28).SetCellValue(auditFile.Header?.CurrencyCode ?? string.Empty);
-                    row.CreateCell(29).SetCellValue("1"); // Taxa de Câmbio fixa como 1
+                    row.CreateCell(29).SetCellValue(taxaFixaCambio); // Taxa de Câmbio fixa como 1
                 });
 
                 using var stream = new MemoryStream();
